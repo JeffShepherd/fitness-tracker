65dc05371261dfe87bdba382371eb5b35ef25216
Jeff S <jeff.shep777@gmail.com>
2022-06-02 11:30:09 -0600

    Add and import CSS reset file

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,7 @@
 
 console.log(userData,"<>>>>userData")
 // An example of how you tell webpack to use a CSS file
+import './css/reset.css'
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
